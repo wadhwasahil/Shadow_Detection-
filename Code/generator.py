daef42a4e7805003dc85c36c3cfd66b27f6fb17f
Sahil Wadhwa <sidashsahil@gmail.com>
2018-05-09 01:11:01 +0530

    Adding deconv layers

--- Code/generator.py
+++ Code/generator.py
@@ -14,39 +14,68 @@ class Generator(object):
         self.X = tf.placeholder(tf.float32, [None, 256, 256, 3], name="X_train")
 
         with tf.name_scope("conv64"):
-            conv = default_conv(self.X, 64)
+            conv1 = default_conv(self.X, 64)
 
         with tf.name_scope("conv_RB128"):
-            r = tf.nn.leaky_relu(conv, name="leaky_relu")
-            conv = default_conv(r, 128)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r1 = tf.nn.leaky_relu(conv1, name="leaky_relu")
+            conv2 = default_conv(r1, 128)
+            batch_norm1 = tf.layers.batch_normalization(conv2, training=True)
 
         with tf.name_scope("conv_RB256"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 256)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r3 = tf.nn.leaky_relu(batch_norm1, name="leaky_relu")
+            conv3 = default_conv(r3, 256)
+            batch_norm2 = tf.layers.batch_normalization(conv3, training=True)
 
         with tf.name_scope("conv_RB512_1"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 512)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r4 = tf.nn.leaky_relu(batch_norm2, name="leaky_relu")
+            conv4 = default_conv(r4, 512)
+            batch_norm3 = tf.layers.batch_normalization(conv4, training=True)
 
         with tf.name_scope("conv_RB512_2"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 512)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r5 = tf.nn.leaky_relu(batch_norm3, name="leaky_relu")
+            conv5 = default_conv(r5, 512)
+            batch_norm4 = tf.layers.batch_normalization(conv5, training=True)
 
         with tf.name_scope("conv_RB512_3"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 512)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r6 = tf.nn.leaky_relu(batch_norm4, name="leaky_relu")
+            conv6 = default_conv(r6, 512)
+            batch_norm5 = tf.layers.batch_normalization(conv6, training=True)
 
         with tf.name_scope("conv_RB512_4"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 512)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r7 = tf.nn.leaky_relu(batch_norm5, name="leaky_relu")
+            conv7 = default_conv(r7, 512)
+            batch_norm6 = tf.layers.batch_normalization(conv7, training=True)
 
         with tf.name_scope("conv_RB512_5"):
-            r = tf.nn.leaky_relu(batch_norm, name="leaky_relu")
-            conv = default_conv(r, 512)
-            batch_norm = tf.layers.batch_normalization(conv, training=True)
+            r8 = tf.nn.leaky_relu(batch_norm6, name="leaky_relu")
+            conv8 = default_conv(r8, 512)
+            batch_norm7 = tf.layers.batch_normalization(conv8, training=True)
+
+        with tf.name_scope("deconv_DR512_1"):
+            r9 = tf.nn.relu(batch_norm7, name="relu")
+            deconv1 = tf.layers.conv2d_transpose(r9, 512, kernel_size=(5,5), strides=(2,2), padding="same")
+            batch_norm8 = tf.layers.batch_normalization(deconv1, training=True)
+            dropout1 = tf.nn.dropout(batch_norm8, keep_prob=0.5, name="dropout")
+            concat1 = tf.concat([dropout1, batch_norm6], axis=0)
+
+        with tf.name_scope("deconv_DR512_2"):
+            r10 = tf.nn.relu(concat1, name="relu")
+            deconv2 = tf.layers.conv2d_transpose(r10, 512, kernel_size=(5, 5), strides=(2, 2), padding="same")
+            batch_norm9 = tf.layers.batch_normalization(deconv2, training=True)
+            dropout2 = tf.nn.dropout(batch_norm9, keep_prob=0.5, name="dropout")
+            concat2 = tf.concat([dropout2, batch_norm5], axis=0)
+
+        with tf.name_scope("deconv_DR512_3"):
+            r11 = tf.nn.relu(concat2, name="relu")
+            deconv3 = tf.layers.conv2d_transpose(r11, 512, kernel_size=(5, 5), strides=(2, 2), padding="same")
+            batch_norm10 = tf.layers.batch_normalization(deconv3, training=True)
+            dropout3 = tf.nn.dropout(batch_norm10, keep_prob=0.5, name="dropout")
+            concat3 = tf.concat([dropout3, batch_norm4], axis=0)
+
+        with tf.name_scope("deconv_R512"):
+            r12 = tf.nn.relu(concat3, name="relu")
+            deconv4 = tf.layers.conv2d_transpose(r12, 512, kernel_size=(5, 5), strides=(2, 2), padding="same")
+            batch_norm11 = tf.layers.batch_normalization(deconv4, training=True)
+            concat3 = tf.concat([dropout3, batch_norm11], axis=0)
+
+
